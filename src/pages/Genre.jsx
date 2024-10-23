a249acb8313fc13f3f6563540ba7a54b80bcc2a0
vermin84 <verminator84@gmail.com>
2024-10-23 12:32:37 +0300

    Update Genre.jsx

--- src/pages/Genre.jsx
+++ src/pages/Genre.jsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 import useGetMoviesByGenre from '../hooks/useGetMoviesByGenre'
 import GridContainer from '../ui/GridContainer'
-import Loader from '../ui/loader'
+import Loader from '../ui/Loader'
 import MovieCard from '../ui/MovieCard'
 
 function Genre() {
